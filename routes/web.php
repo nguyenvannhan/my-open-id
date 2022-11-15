28d602a7aecfb9271800697de7ab30f711abb897
Nguyen Van Nhan <nguyenvannhan0810@gmail.com>
2022-11-15 20:16:25 +0700

    Update docker

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,7 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\AuthorizationController;
-use InvalidArgumentException;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Str;
 
