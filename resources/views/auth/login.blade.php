809249e394ddf9cd82b034625e21f3ffc191331f
Nguyen Van Nhan <nguyenvannhan0810@gmail.com>
2022-11-15 16:10:41 +0700

    Finish open id authorize

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,33 @@
+@extends('layout')
+
+@section('content')
+    <div class="tab-content">
+        <div class="tab-pane fade show active" id="pills-login" role="tabpanel" aria-labelledby="tab-login">
+            <form method="POST" action="{{ route('login') }}">
+                @csrf 
+
+                <!-- Email input -->
+                <div class="form-outline mb-4">
+                    <label class="form-label" for="loginName">Email</label>
+                    <input type="email" id="loginName" class="form-control" name="email" />
+                </div>
+
+                <!-- Password input -->
+                <div class="form-outline mb-4">
+                    <label class="form-label" for="loginPassword">Password</label>
+                    <input type="password" id="loginPassword" class="form-control" name="password" />
+                </div>
+
+                @if ($errors->any())
+                    <div class="alert alert-warning">
+                        {{ $errors->first() }}
+                    </div>
+                @endif
+
+                <div class="d-grid gap-1">
+                    <button type="submit" class="btn btn-primary btn-block mb-4">Sign in</button>
+                </div>
+            </form>
+        </div>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,12 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\AuthorizationController;
+use InvalidArgumentException;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Str;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +21,35 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+})->name('home');
+
+Route::get('/auth/login', function (Request $request) {
+    $request->session()->put('state', $state = Str::random(40));
+
+    $request->session()->put(
+        'code_verifier',
+        $code_verifier = Str::random(128)
+    );
+
+    $codeChallenge = strtr(rtrim(
+        base64_encode(hash('sha256', $code_verifier, true)),
+        '='
+    ), '+/', '-_');
+
+    $query = http_build_query([
+        'client_id' => env('CLIENT_ID'),
+        'redirect_uri' => env('REDIRECT_URL'),
+        'response_type' => 'code',
+        'scope' => '',
+        'state' => $state,
+        'code_challenge' => $codeChallenge,
+        'code_challenge_method' => 'S256',
+    ]);
+
+    return redirect('http://localhost/open-id/authorize?'.$query);
 });
+
+Route::view('login', 'auth.login')->name('login');
+Route::post('login', [AuthController::class, 'postLogin']);
+
+Route::get('/open-id/authorize', [AuthorizationController::class, 'authorizeOpenID']);
